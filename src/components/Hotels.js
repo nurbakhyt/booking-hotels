6a09189742693e40edef0be011209ee22ad18700
Nurbakhyt <nurbakhyt@gmail.com>
2019-06-21 00:57:05 +0600

    hotels list & card

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
+import Card from './Hotels/Card';
 
 class Hotels extends Component {
   render() {
     const {hotels} = this.props;
     const count = hotels.length > 0 ? `${hotels.length} отелей` : 'Нет отелей';
+    const cards = hotels.length > 0
+      ? hotels.map(h => <Card key={h.id} hotel={h}/>)
+      : <p>Нет отелей</p>;
 
     return (
       <section className="page">
         <h1 className="page__title">Все отели: <small>{count}</small></h1>
 
+        {cards}
       </section>
     );
   }
--- src/components/Hotels/Card.js
+++ src/components/Hotels/Card.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import './card.scss';
 
 class HotelCard extends Component {
   render() {
-    return (
-      <div>
+    const {hotel} = this.props;
 
-      </div>
+    return (
+      <article className="card">
+        <div className="card-media">
+          <img className="card__image" src={hotel.images[0]} alt={hotel.name} />
+          <span className="card__star">⭐ {hotel.stars}</span>
+          <span className="card__city">{hotel.city}</span>
+        </div>
+        <div className="card-content">
+          <h2 className="card__name">{hotel.name}</h2>
+          <p className="card__description">{hotel.description}</p>
+          <Link to={`/hotels/${hotel.id}`} className="card__link" title={`Забронировать ${hotel.name}`}>Забронировать</Link>
+        </div>
+      </article>
     );
   }
 }
